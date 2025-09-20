988fbf4352a5cb9a2025e2ce9a25136918833142
Hexxtech <110294965+Hexxtech@users.noreply.github.com>
2025-09-20 22:01:47 +0800

    Create data.js

---
+++ seeddb/data.js
@@ -0,0 +1,12 @@
+const serverSeedData = {
+  serverSeedDb: [
+    {
+      serverSeed: "01707b9930c0bba21babd5147aceac2ee18d3136abdafb099e714e7987f4aafc",
+      serverSeedHashed: "407ca67c38df3580b0e1e90ddd414047d93ef33bbb0ba8da0c741d164abf918a"
+    },
+    {
+      serverSeed: "11a560e32e5f50c3f0354602eb16153e70e3233e0fc2461e358dbcd82987e2de",
+      serverSeedHashed: "a92037341f61813cb30c573cbf5616db6f42729811a1325a89cf17bec4a5dadc"
+    }
+  ]
+};
